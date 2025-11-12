074400cfef3159a57edbe84ff2e6ce27fec0af10
Yonas Melkie <yonasmelkie@gmail.com>
2025-11-12 08:46:11 -0800

    Add a click eventlistenter for the calculator and handle some of the senarios

--- assignments/4/scripts/calculator.js
+++ assignments/4/scripts/calculator.js
@@ -1,8 +1,28 @@
 const display = document.querySelector(".display");
 const calculator = document.querySelector(".calculator");
+let num1 = null;
+let num2 = null;
+let opp = null;
+let waitingNum2 = false;
 
 
-calculator.addEventListener("click", (event)=>{
-    console.log("hello")
-    console.log(event.target.getAttribute("data-value"));
+calculator.addEventListener("click", (event) => {
+    const target = event.target;
+
+    if (target.classList.contains("num") || target.classList.contains("decimal")) {
+        if (display.textContent == "0") {
+            display.textContent = target.textContent;
+        } else {
+            display.textContent += target.textContent;
+        }
+    } else if (target.classList.contains("clear")) {
+        display.textContent = "0";
+    } else if (target.classList.contains("backspace")) {
+        if (display.textContent.length === 1) {
+            display.textContent = "0";
+        } else {
+            display.textContent = display.textContent.slice(0, -1);
+        }
+    }
+
 });
