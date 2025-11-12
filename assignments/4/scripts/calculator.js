d53c89fcc487df636663826d87a633defdfc1b1c
Yonas Melkie <yonasmelkie@gmail.com>
2025-11-12 11:26:15 -0800

    complete the logic for all opperations

--- assignments/4/scripts/calculator.js
+++ assignments/4/scripts/calculator.js
@@ -4,6 +4,7 @@ let num1 = null;
 let num2 = null;
 let opp = null;
 let waitingNum2 = false;
+let resultShown = false;
 
 
 const add = (num1, num2) => {
@@ -17,7 +18,7 @@ const mult = (num1, num2) => {
 }
 const div = (num1, num2) => {
     if (num2 === 0) {
-        return "Err!"
+        return "Err!";
     } else {
         return num1 / num2;
     }
@@ -27,10 +28,10 @@ const percent = (num1, num2) => {
 }
 
 const calc = (num1, num2, opp) => {
-    if (isNaN(num1) || isNaN(num2)){
+    if (isNaN(num1) || isNaN(num2)) {
         return "Err!";
     }
-    switch (opp){
+    switch (opp) {
         case "+":
             return add(num1, num2);
         case "-":
@@ -51,8 +52,15 @@ calculator.addEventListener("click", (event) => {
     const target = event.target;
 
     if (target.classList.contains("num") || target.classList.contains("decimal")) {
-        if (display.textContent == "0") {
+        if (waitingNum2) {
+            display.textContent = "";
+            waitingNum2 = false;
+        }
+        if (display.textContent == "0" || resultShown) {
             display.textContent = target.textContent;
+            if (resultShown){
+                resultShown = false;
+            }
         } else {
             display.textContent += target.textContent;
         }
@@ -64,6 +72,17 @@ calculator.addEventListener("click", (event) => {
         } else {
             display.textContent = display.textContent.slice(0, -1);
         }
+    } else if (target.classList.contains("opp")) {
+        if (!waitingNum2) {
+            num1 = Number(display.textContent);
+            opp = target.getAttribute("data-value");
+            waitingNum2 = true;
+        }
+    } else if (target.classList.contains("equals")){
+        num2 = Number(display.textContent);
+        let result = calc(num1, num2, opp);
+        display.textContent = result.toString();
+        resultShown = true;
     }
 
 });
