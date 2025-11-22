7c2ef468fa99bd48a76250d8e598c1194061b2d2
Yonas Melkie <yonasmelkie@gmail.com>
2025-11-22 14:28:43 -0800

    Set up the database by adding 5 tables

---
+++ assignments/5/database/schema.sql
@@ -0,0 +1,301 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict sh2eEhzA3xwRTUWdON0NsluTGsAo5uVeAEDveMucklRZSqdfeC3JBN4Ocn3zLBN
+
+-- Dumped from database version 16.11 (Homebrew)
+-- Dumped by pg_dump version 16.11 (Homebrew)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: comments; Type: TABLE; Schema: public; Owner: yonasmelkie
+--
+
+CREATE TABLE public.comments (
+    comment_id integer NOT NULL,
+    user_id integer NOT NULL,
+    post_id integer NOT NULL,
+    content character varying(280) NOT NULL,
+    created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP
+);
+
+
+ALTER TABLE public.comments OWNER TO yonasmelkie;
+
+--
+-- Name: comments_comment_id_seq; Type: SEQUENCE; Schema: public; Owner: yonasmelkie
+--
+
+CREATE SEQUENCE public.comments_comment_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.comments_comment_id_seq OWNER TO yonasmelkie;
+
+--
+-- Name: comments_comment_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: yonasmelkie
+--
+
+ALTER SEQUENCE public.comments_comment_id_seq OWNED BY public.comments.comment_id;
+
+
+--
+-- Name: follows; Type: TABLE; Schema: public; Owner: yonasmelkie
+--
+
+CREATE TABLE public.follows (
+    following_id integer NOT NULL,
+    follower_id integer NOT NULL,
+    created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP
+);
+
+
+ALTER TABLE public.follows OWNER TO yonasmelkie;
+
+--
+-- Name: likes; Type: TABLE; Schema: public; Owner: yonasmelkie
+--
+
+CREATE TABLE public.likes (
+    post_id integer NOT NULL,
+    user_id integer NOT NULL,
+    created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP
+);
+
+
+ALTER TABLE public.likes OWNER TO yonasmelkie;
+
+--
+-- Name: posts; Type: TABLE; Schema: public; Owner: yonasmelkie
+--
+
+CREATE TABLE public.posts (
+    post_id integer NOT NULL,
+    user_id integer NOT NULL,
+    content character varying(280) NOT NULL,
+    created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP
+);
+
+
+ALTER TABLE public.posts OWNER TO yonasmelkie;
+
+--
+-- Name: posts_post_id_seq; Type: SEQUENCE; Schema: public; Owner: yonasmelkie
+--
+
+CREATE SEQUENCE public.posts_post_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.posts_post_id_seq OWNER TO yonasmelkie;
+
+--
+-- Name: posts_post_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: yonasmelkie
+--
+
+ALTER SEQUENCE public.posts_post_id_seq OWNED BY public.posts.post_id;
+
+
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: yonasmelkie
+--
+
+CREATE TABLE public.users (
+    user_id integer NOT NULL,
+    user_name character varying(50) NOT NULL,
+    email character varying(100) NOT NULL,
+    age integer,
+    bio text,
+    created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT users_age_check CHECK ((age >= 13))
+);
+
+
+ALTER TABLE public.users OWNER TO yonasmelkie;
+
+--
+-- Name: users_user_id_seq; Type: SEQUENCE; Schema: public; Owner: yonasmelkie
+--
+
+CREATE SEQUENCE public.users_user_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.users_user_id_seq OWNER TO yonasmelkie;
+
+--
+-- Name: users_user_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: yonasmelkie
+--
+
+ALTER SEQUENCE public.users_user_id_seq OWNED BY public.users.user_id;
+
+
+--
+-- Name: comments comment_id; Type: DEFAULT; Schema: public; Owner: yonasmelkie
+--
+
+ALTER TABLE ONLY public.comments ALTER COLUMN comment_id SET DEFAULT nextval('public.comments_comment_id_seq'::regclass);
+
+
+--
+-- Name: posts post_id; Type: DEFAULT; Schema: public; Owner: yonasmelkie
+--
+
+ALTER TABLE ONLY public.posts ALTER COLUMN post_id SET DEFAULT nextval('public.posts_post_id_seq'::regclass);
+
+
+--
+-- Name: users user_id; Type: DEFAULT; Schema: public; Owner: yonasmelkie
+--
+
+ALTER TABLE ONLY public.users ALTER COLUMN user_id SET DEFAULT nextval('public.users_user_id_seq'::regclass);
+
+
+--
+-- Name: comments comments_pkey; Type: CONSTRAINT; Schema: public; Owner: yonasmelkie
+--
+
+ALTER TABLE ONLY public.comments
+    ADD CONSTRAINT comments_pkey PRIMARY KEY (comment_id);
+
+
+--
+-- Name: follows followers_pkey; Type: CONSTRAINT; Schema: public; Owner: yonasmelkie
+--
+
+ALTER TABLE ONLY public.follows
+    ADD CONSTRAINT followers_pkey PRIMARY KEY (follower_id, following_id);
+
+
+--
+-- Name: likes likes_pkey; Type: CONSTRAINT; Schema: public; Owner: yonasmelkie
+--
+
+ALTER TABLE ONLY public.likes
+    ADD CONSTRAINT likes_pkey PRIMARY KEY (user_id, post_id);
+
+
+--
+-- Name: posts posts_pkey; Type: CONSTRAINT; Schema: public; Owner: yonasmelkie
+--
+
+ALTER TABLE ONLY public.posts
+    ADD CONSTRAINT posts_pkey PRIMARY KEY (post_id);
+
+
+--
+-- Name: users users_email_key; Type: CONSTRAINT; Schema: public; Owner: yonasmelkie
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_email_key UNIQUE (email);
+
+
+--
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: yonasmelkie
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (user_id);
+
+
+--
+-- Name: users users_user_name_key; Type: CONSTRAINT; Schema: public; Owner: yonasmelkie
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_user_name_key UNIQUE (user_name);
+
+
+--
+-- Name: comments comments_post_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: yonasmelkie
+--
+
+ALTER TABLE ONLY public.comments
+    ADD CONSTRAINT comments_post_id_fkey FOREIGN KEY (post_id) REFERENCES public.posts(post_id) ON DELETE CASCADE;
+
+
+--
+-- Name: comments comments_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: yonasmelkie
+--
+
+ALTER TABLE ONLY public.comments
+    ADD CONSTRAINT comments_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(user_id) ON DELETE CASCADE;
+
+
+--
+-- Name: follows followers_follower_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: yonasmelkie
+--
+
+ALTER TABLE ONLY public.follows
+    ADD CONSTRAINT followers_follower_id_fkey FOREIGN KEY (follower_id) REFERENCES public.users(user_id) ON DELETE CASCADE;
+
+
+--
+-- Name: follows followers_following_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: yonasmelkie
+--
+
+ALTER TABLE ONLY public.follows
+    ADD CONSTRAINT followers_following_id_fkey FOREIGN KEY (following_id) REFERENCES public.users(user_id) ON DELETE CASCADE;
+
+
+--
+-- Name: likes likes_post_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: yonasmelkie
+--
+
+ALTER TABLE ONLY public.likes
+    ADD CONSTRAINT likes_post_id_fkey FOREIGN KEY (post_id) REFERENCES public.posts(post_id) ON DELETE CASCADE;
+
+
+--
+-- Name: likes likes_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: yonasmelkie
+--
+
+ALTER TABLE ONLY public.likes
+    ADD CONSTRAINT likes_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(user_id) ON DELETE CASCADE;
+
+
+--
+-- Name: posts posts_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: yonasmelkie
+--
+
+ALTER TABLE ONLY public.posts
+    ADD CONSTRAINT posts_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(user_id) ON DELETE CASCADE;
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict sh2eEhzA3xwRTUWdON0NsluTGsAo5uVeAEDveMucklRZSqdfeC3JBN4Ocn3zLBN
+
