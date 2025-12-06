97ad20961c1ad61e566339143d153a8c42eadcc1
Yonas Melkie <yonasmelkie@gmail.com>
2025-12-06 15:52:22 -0800

    Check if the user owns the post before making any changes to it

--- assignments/5/middlewares/auth.js
+++ assignments/5/middlewares/auth.js
@@ -19,4 +19,28 @@ const authenticateToken = (req, res, next) => {
     });
 };
 
-module.exports = { authenticateToken };
+//make sure requester owns the post
+const checkPostOwnership = async (req, res, next) => {
+    try{
+        const { id } = req.params;
+        const result = await db.query(
+            'SELECT user_id FROM posts WHERE post_id = $1', [id]
+        );
+
+        if (result.rows.length === 0){
+            return res.status(404).json({error: 'post not found'});
+        }
+
+        if (result.rows[0].user_id !== req.user_id){
+            return res.status(403).json({error: 'can not modify others post'});
+        }
+        next();
+    }catch(err){
+        console.error(err);
+        res.status(500).json({error: 'database error'});
+    }
+};
+module.exports = { 
+    authenticateToken,
+    checkPostOwnership
+ };
