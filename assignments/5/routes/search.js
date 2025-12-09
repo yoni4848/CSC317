03bfff553c48850ee69aeacda8b3b42f58af3726
Yonas Melkie <yonasmelkie@gmail.com>
2025-12-09 11:25:28 -0800

    add endpoint for searching users using their username

--- assignments/5/routes/search.js
+++ assignments/5/routes/search.js
@@ -23,4 +23,26 @@ router.get('/posts', async (req, res) => {
     }
 });
 
+
+//search for users using their usename
+
+router.get('/users', async (req, res) => {
+    try{
+        const { q } = req.query;
+        if(!q){
+            return res.status(400).json({error: 'missing a keyword to search'});
+        }
+        const keyword = `%${q}%`;
+
+        const result = await db.query(
+            'SELECT username, user_id, bio, profile_picture, created_at FROM users WHERE username ILIKE $1', [keyword]
+        );
+        res.status(200).json(result.rows);
+
+    } catch(err){
+        console.error(err);
+        res.status(500).json({error: 'database error'});
+    }
+})
+
 module.exports = router;
